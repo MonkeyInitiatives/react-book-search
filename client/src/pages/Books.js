cf7029f0690e24564b52b2d47798bf42b6375d6e
Thomas Woods <>
2019-04-11 10:53:21 -0400

    fixed card text

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -87,7 +87,7 @@ class Books extends Component {
                                     <ViewBtn />
                                   </Link>
                                   </h5>
-                                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                  <p className="card-text">{book.synopsis}</p>
                                   <p className="card-text"><small className="text-muted">Written by {book.author}</small></p>
                               </div>
                           </div>
