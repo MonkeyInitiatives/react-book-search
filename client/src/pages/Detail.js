10bd6c52718e668023cbdcdd9e1da03dd0c481a6
Thomas Woods <>
2019-04-11 12:02:24 -0400

    renamed title

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -40,7 +40,7 @@ class Detail extends Component {
         </Row>
         <Row>
           <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
+            <Link to="/">← Back to Books</Link>
           </Col>
         </Row>
       </Container>
